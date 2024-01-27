811330bd4b420d5681888c22ad277144e1c46158
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-01-27 17:23:03 +1000

    Задача №5 Вывод на экран чисел от 1 до N

---
+++ Work5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №5 Дано натуральное число N. Вывести на экран числа от 1 до N.
+
+int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+ //   Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
